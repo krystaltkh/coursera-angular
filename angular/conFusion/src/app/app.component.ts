b232241039b73db67fd58febaf9c7e64609385b2
Krystal Tay <e0425873@u.nus.edu>
2022-05-21 11:05:41 +0800

    Components Part 1

--- angular/conFusion/src/app/app.component.ts
+++ angular/conFusion/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // separate template file for clean code
+  // template: `<h1>{{title}}</h1>`, // in-line template
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
---
+++ angular/conFusion/src/app/shared/dish.ts
@@ -0,0 +1,12 @@
+// create a typescript class
+
+export class Dish {
+    id: string;
+    name: string;
+    image: string; // URL of image
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
